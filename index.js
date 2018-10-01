c6651ff3c3160a53448192bb6261e8ce67df02f9
Barbara Carvalho <bacs96@gmail.com>
2018-10-01 14:58:59 -0300

    get @here messages at slack

--- index.js
+++ index.js
@@ -16,22 +16,25 @@ app.get('/github_info', (req, res) => {
     ).then(res.send(ndssj))
 });
 
-app.listen('4390', function () {
-    console.log('listening on 4390');
+app.listen('4392', function () {
+    console.log('listening on 4392');
     console.log(process.env.SLACK_BASE);
 });
-
+// https://api.slack.com/methods/search.messages
 app.get('/auth', (req, res) => {
     var str = process.env.SLACK_BASE + process.env.SLACK_SEARCH_MSG 
     + '?token=xoxp-417514605056-419276193895-419538005029-3bb9bcd489238edd34f2fbb89ec9e9f7'
+    + '&count=1'
     + '&query=@here'
     + '&highlights=true';
 
     axios.get(
         encodeURI(str)    
     ).then(result => {
-        console.log('@here: ', result);
-        res.json(result.data);
+        var response = {
+            channel: result.data.messages.matches[0].channel.name,
+            message: result.data.messages.matches[0].text,
+        };
+        res.json(response);
     });
-    
 });
