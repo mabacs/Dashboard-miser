3d47994635f45bd17e9555be3a9e0991fc2bc17d
Barbara Carvalho <bacs96@gmail.com>
2018-10-02 20:15:21 -0300

    getting slack informations

--- index.js
+++ index.js
@@ -20,8 +20,10 @@ app.listen('4392', function () {
     console.log('listening on 4392');
     console.log(process.env.SLACK_BASE);
 });
+
 // https://api.slack.com/methods/search.messages
-app.get('/auth', (req, res) => {
+// get messages with @here
+app.get('/here', (req, res) => {
     var str = process.env.SLACK_BASE + process.env.SLACK_SEARCH_MSG 
     + '?token=xoxp-417514605056-419276193895-419538005029-3bb9bcd489238edd34f2fbb89ec9e9f7'
     + '&count=1'
@@ -36,5 +38,57 @@ app.get('/auth', (req, res) => {
             message: result.data.messages.matches[0].text,
         };
         res.json(response);
+    });
+});
+
+// https://api.slack.com/methods/conversations.list
+// get channels info
+// app.get('/channel', (req, res) => {
+//     var str = process.env.SLACK_BASE + process.env.SLACK_CHANNELS
+//     + '?token=xoxp-417514605056-419276193895-419538005029-3bb9bcd489238edd34f2fbb89ec9e9f7'
+
+//     axios.get(
+//         encodeURI(str)    
+//     ).then(result => {        
+//         res.json(result.data.channels);
+//     });
+// });
+
+// general id = CCAB2Q0P7
+// random id = CCB7XG6TH
+
+// https://api.slack.com/methods/channels.history
+// get messages from channel 'general'
+app.get('/channel', (req, res) => {
+    var str = process.env.SLACK_BASE + process.env.SLACK_CHANNEL_MSG
+    + '?token=xoxp-417514605056-419276193895-419538005029-3bb9bcd489238edd34f2fbb89ec9e9f7'
+    + '&channel=CCAB2Q0P7'
+    + '&count=3'
+
+    axios.get(
+        encodeURI(str)    
+    ).then(result => {        
+        res.json(result.data.messages);        
+    });
+});
+
+
+// SPOTIFY
+
+var client_id = '5487af725f244e75867da5c13c76edf7';
+var client_secret = 'ef4ae0b316de4fdd9bd991cc959b4685';
+var redirect_uri = '';
+
+app.get('/spotify', (req, res) => {
+    var str = process.env.SPOTIFY_AUTH
+    + '?client_id=' + client_id
+    + '&redirect_uri=http://127.0.0.1:4392/spotify'
+    + '&response_type=token'
+    + '&currently-playing'
+
+    axios.get(
+        encodeURI(str)    
+    ).then(result => {
+        res.json(result.data);        
     });
 });
