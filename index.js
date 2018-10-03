80576c9f9af1f6725a7a561f6ec628b98932cc71
Barbara Carvalho <bacs96@gmail.com>
2018-10-03 00:41:24 -0300

    getting oauth token

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const axios = require('axios');
+const querystring = require('querystring');
+
+var slackToken = '';
 
 app.use(bodyParser.json());
 
@@ -10,22 +13,47 @@ app.get('/', function (req, res) {
     res.send('Miser Test');
 });
 
-app.get('/github_info', (req, res) => {
-    axios.get(
-
-    ).then(res.send(ndssj))
-});
-
 app.listen('4392', function () {
     console.log('listening on 4392');
-    console.log(process.env.SLACK_BASE);
+});
+
+
+app.get('/auth', (req, res) => {
+    console.log('auth');
+    
+    let str = process.env.SLACK_AUTH 
+        + '?client_id='
+        + process.env.SLACK_CLIENT_ID
+        + '&scope='
+        + process.env.SLACK_SCOPE_SEARCH_READ + ' ' 
+        + process.env.SLACK_SCOPE_CHANNEL_READ + ' '
+        + process.env.SLACK_SCOPE_CHANNEL_HISTORY;
+            
+    res.redirect(301, encodeURI(str));
+});
+
+app.get('/auth/code', (req, res) => {
+    let data = querystring.stringify({
+        client_id: encodeURI(process.env.SLACK_CLIENT_ID),
+        client_secret: encodeURI(process.env.SLACK_CLIENT_SECRET),
+        code: encodeURI(req.query.code),
+    });
+
+    axios.post(process.env.SLACK_AUTH_ACCESS, data, {
+        headers: {
+            'Content-type': 'application/x-www-form-urlencoded'
+        }
+    }).then(response => {
+        slackToken = response.data.access_token;
+        res.send(response.data.access_token);
+    });
 });
 
 // https://api.slack.com/methods/search.messages
 // get messages with @here
 app.get('/here', (req, res) => {
     var str = process.env.SLACK_BASE + process.env.SLACK_SEARCH_MSG 
-    + '?token=xoxp-417514605056-419276193895-419538005029-3bb9bcd489238edd34f2fbb89ec9e9f7'
+    + '?token=' + slackToken
     + '&count=1'
     + '&query=@here'
     + '&highlights=true';
@@ -39,13 +67,14 @@ app.get('/here', (req, res) => {
         };
         res.json(response);
     });
-});
+})
+
 
 // https://api.slack.com/methods/conversations.list
 // get channels info
 // app.get('/channel', (req, res) => {
 //     var str = process.env.SLACK_BASE + process.env.SLACK_CHANNELS
-//     + '?token=xoxp-417514605056-419276193895-419538005029-3bb9bcd489238edd34f2fbb89ec9e9f7'
+//     + '?token=' + slackToken
 
 //     axios.get(
 //         encodeURI(str)    
@@ -61,7 +90,7 @@ app.get('/here', (req, res) => {
 // get messages from channel 'general'
 app.get('/channel', (req, res) => {
     var str = process.env.SLACK_BASE + process.env.SLACK_CHANNEL_MSG
-    + '?token=xoxp-417514605056-419276193895-419538005029-3bb9bcd489238edd34f2fbb89ec9e9f7'
+    + '?token=' + slackToken
     + '&channel=CCAB2Q0P7'
     + '&count=3'
 
@@ -69,26 +98,5 @@ app.get('/channel', (req, res) => {
         encodeURI(str)    
     ).then(result => {        
         res.json(result.data.messages);        
-    });
-});
-
-
-// SPOTIFY
-
-var client_id = '5487af725f244e75867da5c13c76edf7';
-var client_secret = 'ef4ae0b316de4fdd9bd991cc959b4685';
-var redirect_uri = '';
-
-app.get('/spotify', (req, res) => {
-    var str = process.env.SPOTIFY_AUTH
-    + '?client_id=' + client_id
-    + '&redirect_uri=http://127.0.0.1:4392/spotify'
-    + '&response_type=token'
-    + '&currently-playing'
-
-    axios.get(
-        encodeURI(str)    
-    ).then(result => {
-        res.json(result.data);        
     });
 });
